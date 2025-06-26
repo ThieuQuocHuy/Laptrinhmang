b6f492205164a4a4411596213240c9b8a6eec8a0
ThieuQuocHuy <fcfvbuhj2001@gmail.com>
2025-06-26 23:18:07 +0700

    fix: Bid.cs

--- Shared/Models/Bid.cs
+++ Shared/Models/Bid.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿namespace Shared.Models
-=======
 namespace Shared.Models
->>>>>>> 1d5b037f2a55061f8047cad0d58333221eeea5cd
 {
     public class Bid
     {
@@ -10,10 +6,6 @@ namespace Shared.Models
         public int AuctionId { get; set; }
         public int UserId { get; set; }
         public decimal Amount { get; set; }
-<<<<<<< HEAD
         public DateTime BidTime { get; set; }
-=======
-        public DateTime BidTime { get; set; }//..//
->>>>>>> 1d5b037f2a55061f8047cad0d58333221eeea5cd
     }
 }
