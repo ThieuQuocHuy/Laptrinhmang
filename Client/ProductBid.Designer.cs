10c670541e836e300f7428e703e602c43f17b4e6
nguyenluongquy136 <nguyenquy2004py@gmail.com>
2025-06-29 21:27:48 +0700

    Add ProductBid related files

---
+++ Client/ProductBid.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace client
+{
+    partial class ProductBid
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label lblTieuDeDauGia;
+        private Label lblBienSoXeTieuDe;
+        private Label lblBienSo;
+        private Label lblGiaHienTaiTieuDe;
+        private Label lblGiaHT;
+        private Label lblTGTieuDe;
+        private Label lblTG;
+        private TextBox txtGiaMoi;
+        private Button btnDatGia;
+        private Label lblLichSuDauGia;
+        private Panel mainPanel;
+        private CheckBox chkAutoBid;
+        private TextBox txtMaxBid;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            mainPanel = new Panel();
+            lvHistory = new ListView();
+            lblTieuDeDauGia = new Label();
+            lblBienSoXeTieuDe = new Label();
+            lblBienSo = new Label();
+            lblGiaHienTaiTieuDe = new Label();
+            lblGiaHT = new Label();
+            lblTGTieuDe = new Label();
+            lblTG = new Label();
+            txtGiaMoi = new TextBox();
+            btnDatGia = new Button();
+            lblLichSuDauGia = new Label();
+            chkAutoBid = new CheckBox();
+            txtMaxBid = new TextBox();
+            menuStrip1 = new MenuStrip();
+            TrangChuToolStripMenuItem = new ToolStripMenuItem();
+            lịchSửToolStripMenuItem = new ToolStripMenuItem();
+            tàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            thôngTinTàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            logOutToolStripMenuItem = new ToolStripMenuItem();
+            splitContainer1 = new SplitContainer();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            mainPanel.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            mainPanel.BackColor = Color.LightSeaGreen;
+            mainPanel.BorderStyle = BorderStyle.FixedSingle;
+            mainPanel.Controls.Add(lvHistory);
+            mainPanel.Controls.Add(lblTieuDeDauGia);
+            mainPanel.Controls.Add(lblBienSoXeTieuDe);
+            mainPanel.Controls.Add(lblBienSo);
+            mainPanel.Controls.Add(lblGiaHienTaiTieuDe);
+            mainPanel.Controls.Add(lblGiaHT);
+            mainPanel.Controls.Add(lblTGTieuDe);
+            mainPanel.Controls.Add(lblTG);
+            mainPanel.Controls.Add(txtGiaMoi);
+            mainPanel.Controls.Add(btnDatGia);
+            mainPanel.Controls.Add(lblLichSuDauGia);
+            mainPanel.Controls.Add(chkAutoBid);
+            mainPanel.Controls.Add(txtMaxBid);
+            mainPanel.Location = new Point(0, 0);
+            mainPanel.Name = "mainPanel";
+            mainPanel.Size = new Size(332, 473);
+            mainPanel.TabIndex = 0;
+            // 
+            // lvHistory
+            // 
+            lvHistory.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lvHistory.GridLines = true;
+            lvHistory.Location = new Point(3, 314);
+            lvHistory.Name = "lvHistory";
+            lvHistory.Size = new Size(324, 154);
+            lvHistory.TabIndex = 10;
+            lvHistory.UseCompatibleStateImageBehavior = false;
+            lvHistory.View = View.Details;
+            // 
+            // lblTieuDeDauGia
+            // 
+            lblTieuDeDauGia.AutoSize = true;
+            lblTieuDeDauGia.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            lblTieuDeDauGia.ForeColor = Color.White;
+            lblTieuDeDauGia.Location = new Point(56, 26);
+            lblTieuDeDauGia.Name = "lblTieuDeDauGia";
+            lblTieuDeDauGia.Size = new Size(227, 30);
+            lblTieuDeDauGia.TabIndex = 0;
+            lblTieuDeDauGia.Text = "ĐẤU GIÁ BIỂN SỐ XE";
+            // 
+            // lblBienSoXeTieuDe
+            // 
+            lblBienSoXeTieuDe.AutoSize = true;
+            lblBienSoXeTieuDe.Font = new Font("Segoe UI", 12F);
+            lblBienSoXeTieuDe.ForeColor = Color.White;
+            lblBienSoXeTieuDe.Location = new Point(3, 86);
+            lblBienSoXeTieuDe.Name = "lblBienSoXeTieuDe";
+            lblBienSoXeTieuDe.Size = new Size(86, 21);
+            lblBienSoXeTieuDe.TabIndex = 1;
+            lblBienSoXeTieuDe.Text = "Biển Số Xe:";
+            // 
+            // lblBienSo
+            // 
+            lblBienSo.AutoSize = true;
+            lblBienSo.BackColor = Color.White;
+            lblBienSo.BorderStyle = BorderStyle.FixedSingle;
+            lblBienSo.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblBienSo.ForeColor = Color.Black;
+            lblBienSo.Location = new Point(113, 67);
+            lblBienSo.Name = "lblBienSo";
+            lblBienSo.Size = new Size(153, 47);
+            lblBienSo.TabIndex = 2;
+            lblBienSo.Text = "ABC-123";
+            // 
+            // lblGiaHienTaiTieuDe
+            // 
+            lblGiaHienTaiTieuDe.AutoSize = true;
+            lblGiaHienTaiTieuDe.Font = new Font("Segoe UI", 12F);
+            lblGiaHienTaiTieuDe.ForeColor = Color.White;
+            lblGiaHienTaiTieuDe.Location = new Point(2, 131);
+            lblGiaHienTaiTieuDe.Name = "lblGiaHienTaiTieuDe";
+            lblGiaHienTaiTieuDe.Size = new Size(96, 21);
+            lblGiaHienTaiTieuDe.TabIndex = 3;
+            lblGiaHienTaiTieuDe.Text = "Giá Hiện Tại:";
+            // 
+            // lblGiaHT
+            // 
+            lblGiaHT.AutoSize = true;
+            lblGiaHT.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblGiaHT.ForeColor = Color.White;
+            lblGiaHT.Location = new Point(162, 131);
+            lblGiaHT.Name = "lblGiaHT";
+            lblGiaHT.Size = new Size(121, 21);
+            lblGiaHT.TabIndex = 4;
+            lblGiaHT.Text = "1.000.000 VND";
+            // 
+            // lblTGTieuDe
+            // 
+            lblTGTieuDe.AutoSize = true;
+            lblTGTieuDe.Font = new Font("Segoe UI", 12F);
+            lblTGTieuDe.ForeColor = Color.White;
+            lblTGTieuDe.Location = new Point(3, 170);
+            lblTGTieuDe.Name = "lblTGTieuDe";
+            lblTGTieuDe.Size = new Size(136, 21);
+            lblTGTieuDe.TabIndex = 5;
+            lblTGTieuDe.Text = "Thời Gian Còn Lại:";
+            // 
+            // lblTG
+            // 
+            lblTG.AutoSize = true;
+            lblTG.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblTG.ForeColor = Color.White;
+            lblTG.Location = new Point(162, 170);
+            lblTG.Name = "lblTG";
+            lblTG.Size = new Size(72, 21);
+            lblTG.TabIndex = 6;
+            lblTG.Text = "00:05:30";
+            // 
+            // txtGiaMoi
+            // 
+            txtGiaMoi.BackColor = Color.White;
+            txtGiaMoi.BorderStyle = BorderStyle.FixedSingle;
+            txtGiaMoi.Font = new Font("Segoe UI", 12F);
+            txtGiaMoi.Location = new Point(2, 209);
+            txtGiaMoi.Name = "txtGiaMoi";
+            txtGiaMoi.PlaceholderText = "Nhập giá tại đây...";
+            txtGiaMoi.Size = new Size(200, 29);
+            txtGiaMoi.TabIndex = 7;
+            txtGiaMoi.KeyPress += txtGiaMoi_KeyPress;
+            // 
+            // btnDatGia
+            // 
+            btnDatGia.BackColor = Color.White;
+            btnDatGia.Cursor = Cursors.Hand;
+            btnDatGia.FlatAppearance.BorderSize = 0;
+            btnDatGia.FlatStyle = FlatStyle.Flat;
+            btnDatGia.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnDatGia.ForeColor = Color.LightSeaGreen;
+            btnDatGia.Location = new Point(219, 208);
+            btnDatGia.Name = "btnDatGia";
+            btnDatGia.Size = new Size(100, 30);
+            btnDatGia.TabIndex = 8;
+            btnDatGia.Text = "Đặt Giá";
+            btnDatGia.UseVisualStyleBackColor = false;
+            btnDatGia.Click += btnDatGia_Click;
+            // 
+            // lblLichSuDauGia
+            // 
+            lblLichSuDauGia.AutoSize = true;
+            lblLichSuDauGia.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            lblLichSuDauGia.ForeColor = Color.White;
+            lblLichSuDauGia.Location = new Point(72, 281);
+            lblLichSuDauGia.Name = "lblLichSuDauGia";
+            lblLichSuDauGia.Size = new Size(194, 30);
+            lblLichSuDauGia.TabIndex = 9;
+            lblLichSuDauGia.Text = "LỊCH SỬ ĐẤU GIÁ";
+            // 
+            // chkAutoBid
+            // 
+            chkAutoBid.AutoSize = true;
+            chkAutoBid.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkAutoBid.ForeColor = Color.White;
+            chkAutoBid.Location = new Point(3, 250);
+            chkAutoBid.Name = "chkAutoBid";
+            chkAutoBid.Size = new Size(136, 25);
+            chkAutoBid.TabIndex = 11;
+            chkAutoBid.Text = "Đặt giá tự động";
+            chkAutoBid.UseVisualStyleBackColor = true;
+            chkAutoBid.CheckedChanged += chkAutoBid_CheckedChanged;
+            // 
+            // txtMaxBid
+            // 
+            txtMaxBid.BackColor = Color.White;
+            txtMaxBid.BorderStyle = BorderStyle.FixedSingle;
+            txtMaxBid.Enabled = false;
+            txtMaxBid.Font = new Font("Segoe UI", 12F);
+            txtMaxBid.Location = new Point(179, 250);
+            txtMaxBid.Name = "txtMaxBid";
+            txtMaxBid.PlaceholderText = "Giá tối đa...";
+            txtMaxBid.Size = new Size(140, 29);
+            txtMaxBid.TabIndex = 12;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { TrangChuToolStripMenuItem, lịchSửToolStripMenuItem, tàiKhoảnToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // TrangChuToolStripMenuItem
+            // 
+            TrangChuToolStripMenuItem.Name = "TrangChuToolStripMenuItem";
+            TrangChuToolStripMenuItem.Size = new Size(71, 20);
+            TrangChuToolStripMenuItem.Text = "Trang chủ";
+            TrangChuToolStripMenuItem.Click += TrangChuToolStripMenuItem_Click;
+            // 
+            // lịchSửToolStripMenuItem
+            // 
+            lịchSửToolStripMenuItem.Name = "lịchSửToolStripMenuItem";
+            lịchSửToolStripMenuItem.Size = new Size(56, 20);
+            lịchSửToolStripMenuItem.Text = "Lịch sử";
+            // 
+            // tàiKhoảnToolStripMenuItem
+            // 
+            tàiKhoảnToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { thôngTinTàiKhoảnToolStripMenuItem, logOutToolStripMenuItem });
+            tàiKhoảnToolStripMenuItem.Name = "tàiKhoảnToolStripMenuItem";
+            tàiKhoảnToolStripMenuItem.Size = new Size(68, 20);
+            tàiKhoảnToolStripMenuItem.Text = "tài khoản";
+            // 
+            // thôngTinTàiKhoảnToolStripMenuItem
+            // 
+            thôngTinTàiKhoảnToolStripMenuItem.Name = "thôngTinTàiKhoảnToolStripMenuItem";
+            thôngTinTàiKhoảnToolStripMenuItem.Size = new Size(175, 22);
+            thôngTinTàiKhoảnToolStripMenuItem.Text = "thông tin tài khoản";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            logOutToolStripMenuItem.Size = new Size(175, 22);
+            logOutToolStripMenuItem.Text = "log out";
+            logOutToolStripMenuItem.Click += logOutToolStripMenuItem_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(0, 27);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(flowLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(mainPanel);
+            splitContainer1.Size = new Size(800, 473);
+            splitContainer1.SplitterDistance = 464;
+            splitContainer1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(464, 473);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // ProductBid
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 500);
+            Controls.Add(menuStrip1);
+            Controls.Add(splitContainer1);
+            Name = "ProductBid";
+            Text = "Đấu Giá Biển Số Xe";
+            Load += ProductBid_Load;
+            mainPanel.ResumeLayout(false);
+            mainPanel.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem TrangChuToolStripMenuItem;
+        private ToolStripMenuItem lịchSửToolStripMenuItem;
+        private ToolStripMenuItem tàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem thôngTinTàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem logOutToolStripMenuItem;
+        private SplitContainer splitContainer1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private ListView lvHistory;
+    }
+}
---
+++ Client/ProductBid.cs
@@ -0,0 +1,367 @@
+﻿using Client;
+using Client.Services;
+using daugia;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class ProductBid : Form
+    {
+        private int _id;
+        private AuctionClient _client;
+        private System.Windows.Forms.Timer _timer;
+        private System.Windows.Forms.Timer autoBidTimer;
+        private Auction auctionItem;
+        private int user_currentbid_id = 0;
+
+        public ProductBid(AuctionClient client, int id)
+        {
+            this._client = client;
+            if (_client != null && !_client.IsConnected())
+            {
+                HomePage homePage = new HomePage(_client, _id);
+                homePage.Show();
+                this.Close();
+                MessageBox.Show("Mất kết nối với server. Vui lòng kiểm tra lại.");
+            }
+            else
+            {
+                InitializeComponent();
+            }
+            _id = id;
+        }
+
+        private async void ProductBid_Load(object sender, EventArgs e)
+        {
+            auctionItem = null;
+            await loadData();
+            await loadListHistory();
+            InitializeTimer();
+            InitializeAutoBidTimer();
+            autoBidTimer.Start();
+            loadListView();
+        }
+
+        //Load dữ liệu sản phẩm
+        private async Task loadData()
+        {
+            try
+            {
+                List<Auction> auctions = await _client.GetActiveAuctions();
+                if (auctionItem == null && auctions.Count > 0)
+                {
+                    auctionItem = auctions[0];
+                }
+                if (auctions.Count > 0)
+                {
+                    lblBienSo.Text = auctionItem.LicensePlateNumber;
+                    lblGiaHT.Text = auctionItem.CurrentPrice.ToString("N0") + " VNĐ";
+                }
+
+                flowLayoutPanel1.Controls.Clear();
+
+                foreach (var auction in auctions)
+                {
+                    var ucProduct = new UCProduct
+                    {
+                        LicensePlateNumber = auction.LicensePlateNumber,
+                        CurrentPrice = auction.CurrentPrice,
+                        StartTime = auction.StartTime,
+                        EndTime = auction.EndTime,
+                        Status = auction.Status
+                    };
+
+                    ucProduct.Clicked += (s, e) => OnItemClicked(auction);
+                    flowLayoutPanel1.Controls.Add(ucProduct);
+                }
+
+                auctions = await _client.GetInactiveAuctions();
+                if (auctionItem == null && auctions.Count > 0)
+                {
+                    auctionItem = auctions[0];
+                    lblBienSo.Text = auctionItem.LicensePlateNumber;
+                    lblGiaHT.Text = auctionItem.CurrentPrice.ToString("N0") + " VNĐ";
+                }
+
+                foreach (var auction in auctions)
+                {
+                    var ucProduct = new UCProduct
+                    {
+                        LicensePlateNumber = auction.LicensePlateNumber,
+                        CurrentPrice = auction.CurrentPrice,
+                        StartTime = auction.StartTime,
+                        EndTime = auction.EndTime,
+                        Status = auction.Status
+                    };
+
+                    ucProduct.Clicked += (s, e) => OnItemClicked(auction);
+                    flowLayoutPanel1.Controls.Add(ucProduct);
+                }
+            }
+            catch (Exception ex)
+            {
+                ProcessError(ex);
+            }
+        }
+
+        private void loadListView()
+        {
+            lvHistory.Columns.Add("Giá đã đặt");
+            lvHistory.Columns.Add("Thời gian");
+        }
+
+        //load lịch sử đấu giá
+        private async Task loadListHistory()
+        {
+            if (auctionItem != null)
+            {
+                try
+                {
+                    lvHistory.Items.Clear();
+                    List<Bid> bids = await _client.GetAuctionBids(auctionItem.Id);
+                    if (bids != null && bids.Count > 0)
+                    {
+                        user_currentbid_id = bids[0].UserId;
+                    }
+                    else
+                    {
+                        user_currentbid_id = 0;
+                    }
+                    
+                    foreach (var bid in bids)
+                    {
+                        var item = new ListViewItem
+                        {
+                            Text = bid.Amount.ToString("N0") + " VNĐ"
+                        };
+                        item.SubItems.Add(new ListViewItem.ListViewSubItem { Text = bid.BidTime.ToString("HH:mm:ss dd/MM/yyyy") });
+                        lvHistory.Items.Add(item);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ProcessError(ex);
+                }
+            }
+            lvHistory.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        //Khi click vào phần tử trong danh sách
+        private void OnItemClicked(Auction auction)
+        {
+            if (!auctionItem.Equals(auction))
+            {
+                auctionItem = auction;
+                lblBienSo.Text = auction.LicensePlateNumber;
+                lblGiaHT.Text = auction.CurrentPrice.ToString("N0") + " VNĐ";
+                autoBidTimer?.Stop();
+                chkAutoBid.Checked = false;
+                txtMaxBid.Text = "";
+            }
+            if (!(_timer.Enabled))
+            {
+                _timer.Start();
+            }
+            UpdateStatus();
+        }
+
+        //chuyển về trang chủ
+        private void TrangChuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HomePage homePage = new HomePage(_client, _id);
+            homePage.Show();
+            this.Close();
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login loginForm = new Login();
+            loginForm.Show();
+            this.Close();
+        }
+
+        private void InitializeTimer()
+        {
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = 1000; // Cập nhật mỗi 1 giây
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateStatus();
+            loadListHistory();
+        }
+
+        //Khởi tạo timer cho chức năng đấu giá tự động
+        private void InitializeAutoBidTimer()
+        {
+            autoBidTimer = new System.Windows.Forms.Timer();
+            autoBidTimer.Interval = 1000; // Kiểm tra mỗi giây
+            autoBidTimer.Tick += AutoBidTimer_Tick;
+        }
+
+        //Tự động đặt giá nếu người dùng tick vào ô checkbox
+        private void AutoBidTimer_Tick(object sender, EventArgs e)
+        {
+            if (chkAutoBid.Checked)
+            {
+                try
+                {
+                    if (chkAutoBid.Checked && decimal.TryParse(txtMaxBid.Text, out decimal maxBid))
+                    {
+                        decimal currentPrice = GetCurrentPrice(); // Lấy giá hiện tại
+                        decimal incrementStep = 100000; // Bước giá
+
+                        if (user_currentbid_id != _id && (currentPrice + incrementStep <= maxBid))
+                        {
+                            decimal newBid = currentPrice + incrementStep;
+                            PlaceBid(newBid);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Lỗi khi đặt giá tự động: {ex.Message}");
+                    autoBidTimer.Stop();
+                }
+
+            }
+        }
+
+        private decimal GetCurrentPrice()
+        {
+            // Hàm giả lập để lấy giá hiện tại từ `auctionItem`
+            return auctionItem != null ? auctionItem.CurrentPrice : 0;
+        }
+
+        //Đặt giá tự động
+        private void PlaceBid(decimal amount)
+        {
+            txtGiaMoi.Text = amount.ToString();
+            btnDatGia.PerformClick(); // Giả lập nhấn nút "Đặt Giá"
+        }
+
+        //Cập nhật trạng thái đấu giá
+        private void UpdateStatus()
+        {
+            DateTime currentTime = DateTime.Now;
+            if (currentTime < auctionItem.StartTime)
+            {
+                if (auctionItem.Status == "Cancelled")
+                {
+                    lblTGTieuDe.Text = "Phiên đấu giá đã bị hủy";
+                    lblTG.Text = "";
+                }
+                else
+                {
+                    lblTGTieuDe.Text = "Bắt đầu sau:";
+                    lblTG.Text = (auctionItem.StartTime - currentTime).ToString(@"hh\:mm\:ss");
+                }
+            }
+            else if (currentTime >= auctionItem.EndTime)
+            {
+                lblTGTieuDe.Text = "Kết thúc lúc:";
+                lblTG.Text = auctionItem.EndTime.ToString("HH:mm:ss dd/MM/yyyy");
+                _timer.Stop(); // Dừng cập nhật khi phiên đấu giá kết thúc
+            }
+            else
+            {
+                lblTGTieuDe.Text = "Kết thúc sau:";
+                lblTG.Text = (auctionItem.EndTime - currentTime).ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        //Xử lý lỗi
+        private void ProcessError(Exception ex)
+        {
+            if (!_client.IsConnected())
+            {
+                _timer?.Stop();
+                _timer?.Dispose();
+                MessageBox.Show("Mất kết nối với server. Vui lòng kiểm tra lại.");
+            }
+            else
+            {
+                MessageBox.Show("Lỗi khi thực hiện thao tác: " + ex.Message);
+            }
+        }
+
+        private void txtGiaMoi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Chỉ cho nhập số từ bàn phím
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private async void btnDatGia_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Kiểm tra kết nối
+                if (!_client.IsConnected())
+                {
+                    MessageBox.Show("Mất kết nối với server. Vui lòng kiểm tra lại.");
+                    return;
+                }
+
+                DateTime currentTime = DateTime.Now;
+                //Kiểm tra điều kiện đặt giá
+                if (currentTime < auctionItem.EndTime && (await _client.GetStatus(auctionItem.Id) == "Active"))
+                {
+                    decimal newBidAmount = decimal.Parse(txtGiaMoi.Text);
+                    if (currentTime >= auctionItem.StartTime && newBidAmount > auctionItem.CurrentPrice)
+                    {
+                        bool isSuccess = await _client.PlaceBid(auctionItem.Id, _client.CurrentUser.Id, newBidAmount);
+                        if (isSuccess)
+                        {
+                            auctionItem.CurrentPrice = newBidAmount;
+                            MessageForm.Show($"Đã đặt giá: {newBidAmount:N0} VNĐ", "Thông báo", 3000);
+                          
+                            txtGiaMoi.Clear();
+                            await loadData(); // Cập nhật lại dữ liệu sau khi đặt giá thành công
+                        }
+                        else
+                        {
+                            MessageBox.Show("Đặt giá thất bại. Vui lòng kiểm tra lại.");
+                        }
+                    }
+                    else if(currentTime < auctionItem.StartTime)
+                    {
+                        MessageBox.Show("Phiên đấu giá chưa diễn ra");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Giá mới phải cao hơn giá hiện tại.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Phiên đấu giá đã kết thúc hoặc bị tạm hoãn");
+                }
+            }
+            catch (Exception ex)
+            {
+                ProcessError(ex);
+
+            }
+        }
+
+        private void chkAutoBid_CheckedChanged(object sender, EventArgs e)
+        {
+            txtMaxBid.Enabled = chkAutoBid.Checked; // Bật/Tắt TextBox giá tối đa
+            if (!chkAutoBid.Checked)
+            {
+                txtMaxBid.Text = ""; // Xóa giá trị nếu hủy kích hoạt
+            }
+            else
+            {
+                autoBidTimer?.Start();
+            }
+        }
+    }
+}
