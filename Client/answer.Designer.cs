71198af323650e47efbc3edb14fc6bc3b9998d8c
ThieuQuocHuy <fcfvbuhj2001@gmail.com>
2025-06-29 19:16:58 +0700

    feat : Text Answer

--- Client/answer.Designer.cs
+++ Client/answer.Designer.cs
@@ -53,43 +53,45 @@
             // 
             // menuStrip1
             // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
             menuStrip1.Items.AddRange(new ToolStripItem[] { trangChủToolStripMenuItem, lịchSửToolStripMenuItem, tàiKhoảnToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Padding = new Padding(7, 3, 0, 3);
+            menuStrip1.Size = new Size(914, 30);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
             // trangChủToolStripMenuItem
             // 
             trangChủToolStripMenuItem.Name = "trangChủToolStripMenuItem";
-            trangChủToolStripMenuItem.Size = new Size(71, 20);
+            trangChủToolStripMenuItem.Size = new Size(87, 24);
             trangChủToolStripMenuItem.Text = "Trang chủ";
             trangChủToolStripMenuItem.Click += trangChủToolStripMenuItem_Click;
             // 
             // lịchSửToolStripMenuItem
             // 
             lịchSửToolStripMenuItem.Name = "lịchSửToolStripMenuItem";
-            lịchSửToolStripMenuItem.Size = new Size(56, 20);
+            lịchSửToolStripMenuItem.Size = new Size(68, 24);
             lịchSửToolStripMenuItem.Text = "Lịch sử";
             // 
             // tàiKhoảnToolStripMenuItem
             // 
             tàiKhoảnToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { thôngTinTàiKhoảnToolStripMenuItem, logOutToolStripMenuItem });
             tàiKhoảnToolStripMenuItem.Name = "tàiKhoảnToolStripMenuItem";
-            tàiKhoảnToolStripMenuItem.Size = new Size(69, 20);
+            tàiKhoảnToolStripMenuItem.Size = new Size(85, 24);
             tàiKhoảnToolStripMenuItem.Text = "Tài khoản";
             // 
             // thôngTinTàiKhoảnToolStripMenuItem
             // 
             thôngTinTàiKhoảnToolStripMenuItem.Name = "thôngTinTàiKhoảnToolStripMenuItem";
-            thôngTinTàiKhoảnToolStripMenuItem.Size = new Size(180, 22);
+            thôngTinTàiKhoảnToolStripMenuItem.Size = new Size(220, 26);
             thôngTinTàiKhoảnToolStripMenuItem.Text = "Thông tin tài khoản";
             // 
             // logOutToolStripMenuItem
             // 
             logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
-            logOutToolStripMenuItem.Size = new Size(180, 22);
+            logOutToolStripMenuItem.Size = new Size(220, 26);
             logOutToolStripMenuItem.Text = "Log out";
             logOutToolStripMenuItem.Click += logOutToolStripMenuItem_Click;
             // 
@@ -105,9 +107,10 @@
             panel1.Controls.Add(textBox2);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(0, 27);
+            panel1.Location = new Point(0, 36);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(800, 446);
+            panel1.Size = new Size(914, 595);
             panel1.TabIndex = 1;
             // 
             // panel2
@@ -115,9 +118,10 @@
             panel2.BackColor = SystemColors.HotTrack;
             panel2.Controls.Add(label5);
             panel2.Controls.Add(label6);
-            panel2.Location = new Point(0, 3);
+            panel2.Location = new Point(0, 4);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(797, 91);
+            panel2.Size = new Size(911, 121);
             panel2.TabIndex = 10;
             panel2.Paint += panel2_Paint;
             // 
@@ -125,9 +129,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(275, 14);
+            label5.Location = new Point(314, 19);
             label5.Name = "label5";
-            label5.Size = new Size(232, 32);
+            label5.Size = new Size(285, 41);
             label5.TabIndex = 8;
             label5.Text = "Đấu Giá Biển Số Xe";
             // 
@@ -135,9 +139,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point, 0);
-            label6.Location = new Point(364, 46);
+            label6.Location = new Point(416, 61);
             label6.Name = "label6";
-            label6.Size = new Size(54, 17);
+            label6.Size = new Size(70, 23);
             label6.TabIndex = 9;
             label6.Text = "Nhóm 2";
             // 
@@ -145,9 +149,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label4.Location = new Point(12, 341);
+            label4.Location = new Point(14, 455);
             label4.Name = "label4";
-            label4.Size = new Size(404, 17);
+            label4.Size = new Size(519, 23);
             label4.TabIndex = 7;
             label4.Text = "* Tôi có thể đợi bao lâu trước khi biển số của tôi được chuyển đi ?";
             label4.Click += label4_Click;
@@ -157,11 +161,12 @@
             textBox3.BackColor = SystemColors.ControlDark;
             textBox3.BorderStyle = BorderStyle.None;
             textBox3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox3.Location = new Point(27, 368);
+            textBox3.Location = new Point(31, 491);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
-            textBox3.Size = new Size(721, 67);
+            textBox3.Size = new Size(824, 89);
             textBox3.TabIndex = 6;
             textBox3.Text = resources.GetString("textBox3.Text");
             textBox3.TextChanged += textBox3_TextChanged;
@@ -170,9 +175,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label3.Location = new Point(12, 248);
+            label3.Location = new Point(14, 331);
             label3.Name = "label3";
-            label3.Size = new Size(378, 17);
+            label3.Size = new Size(486, 23);
             label3.TabIndex = 5;
             label3.Text = "* Việc vận chuyển diễn ra như thế nào và chi phí bao nhiêu ?";
             // 
@@ -181,11 +186,12 @@
             textBox1.BackColor = SystemColors.ControlDark;
             textBox1.BorderStyle = BorderStyle.None;
             textBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(27, 275);
+            textBox1.Location = new Point(31, 367);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
-            textBox1.Size = new Size(721, 48);
+            textBox1.Size = new Size(824, 64);
             textBox1.TabIndex = 4;
             textBox1.Text = resources.GetString("textBox1.Text");
             // 
@@ -194,11 +200,12 @@
             textBox2.BackColor = SystemColors.ControlDark;
             textBox2.BorderStyle = BorderStyle.None;
             textBox2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(27, 183);
+            textBox2.Location = new Point(31, 244);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
-            textBox2.Size = new Size(721, 48);
+            textBox2.Size = new Size(824, 64);
             textBox2.TabIndex = 3;
             textBox2.Text = resources.GetString("textBox2.Text");
             textBox2.TextChanged += textBox2_TextChanged;
@@ -207,9 +214,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label2.Location = new Point(12, 156);
+            label2.Location = new Point(14, 208);
             label2.Name = "label2";
-            label2.Size = new Size(217, 17);
+            label2.Size = new Size(279, 23);
             label2.TabIndex = 1;
             label2.Text = "* Đấu giá hoạt động như thế nào ?";
             // 
@@ -217,20 +224,21 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            label1.Location = new Point(3, 110);
+            label1.Location = new Point(3, 147);
             label1.Name = "label1";
-            label1.Size = new Size(233, 25);
+            label1.Size = new Size(299, 32);
             label1.TabIndex = 0;
             label1.Text = "Các câu hỏi thường gặp :";
             // 
             // answer
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 474);
+            ClientSize = new Size(914, 632);
             Controls.Add(panel1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "answer";
             Text = "answer";
             menuStrip1.ResumeLayout(false);
